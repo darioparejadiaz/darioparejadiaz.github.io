643b68dc68dfb21947fde3b8e6cf0e65fc7aeb39
darioparejadiaz <darioparejadiaz@gmail.com>
2022-08-10 12:25:46 -0500

    README added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dario Pareja Web Page
+
+## Home
+
+### Desktop view
+
+[!Home Desktop view](/assets/readme-imgs/home-desktop.jpg)
--- js/photography-images.js
+++
@@ -1,9 +0,0 @@
-const path = "/assets/photos/";
-
-export const images = [
-  `${path}/Eiffel.jpg`,
-  `${path}/Imperium.jpg`,
-  `${path}/Forza.jpg`,
-  `${path}/Infinity.jpg`,
-  `${path}/Counter-Attack.jpg`,
-];
